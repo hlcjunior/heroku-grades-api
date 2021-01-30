9e6402cc84d83a4e33fb581f4932576b608bffd3
hlcjunior <62648013+hlcjunior@users.noreply.github.com>
2021-01-30 18:12:58 -0300

    CORS ok

--- app.js
+++ app.js
@@ -25,12 +25,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
     cors({
-        /*
-        origin: 'http://localhost:8080',
-        origin: 'http://localhost:8081',
-        origin: 'https://hlcjunior-grades-api.herokuapp.com/',
-        */
-        //origin: '*',
+        //origin: 'http://localhost:8080',
         origin: 'https://hlcjunior-grades-app.herokuapp.com',
     })
 );
